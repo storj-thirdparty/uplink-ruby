a919c0cbd9a6f53f7f475a1c4c4e2c1b0ddb7845
s101d1 <s101d1@users.noreply.github.com>
2023-06-15 04:57:17 +0700

    Added more Access related uplink-c functions (#3)
    * Rename the gemspec file name to make it consistent with the repository name
    
    * Update README.md
    
    * Separate UplinkLib module into own file
    
    * Reorganize the class and function order to follow uplink-c struct and function ordering
    
    * Added more Access related uplink-c functions

--- lib/uplink_lib.rb
+++ lib/uplink_lib.rb
@@ -11,20 +11,7 @@ module UplinkLib
     layout :_handle, :size_t
   end
 
-  class UplinkEncryptionKey < FFI::Struct
-    layout :_handle, :size_t
-  end
-
-  class UplinkBucket < FFI::Struct
-    layout :name, :string,
-           :created, :int64_t
-  end
-
   class UplinkProject < FFI::Struct
-    layout :_handle, :size_t
-  end
-
-  class UplinkUpload < FFI::Struct
     layout :_handle, :size_t
   end
 
@@ -32,17 +19,18 @@ module UplinkLib
     layout :_handle, :size_t
   end
 
-  class UplinkObjectIterator < FFI::Struct
+  class UplinkUpload < FFI::Struct
     layout :_handle, :size_t
   end
 
-  class UplinkUploadOptions < FFI::Struct
-    layout :expires, :int64_t
+  class UplinkEncryptionKey < FFI::Struct
+    layout :_handle, :size_t
   end
 
-  class UplinkDownloadOptions < FFI::Struct
-    layout :offset, :int64_t,
-           :length, :int64_t
+
+  class UplinkBucket < FFI::Struct
+    layout :name, :string,
+           :created, :int64_t
   end
 
   class UplinkSystemMetadata < FFI::Struct
@@ -63,13 +51,20 @@ module UplinkLib
            :count, :size_t
   end
 
-  class UplinkUploadObjectMetadataOptions < FFI::Struct; end
-
   class UplinkObject < FFI::Struct
     layout :key, :string,
            :is_prefix, :bool,
            :system, UplinkSystemMetadata.val,
            :custom, UplinkCustomMetadata.val
+  end
+
+  class UplinkUploadOptions < FFI::Struct
+    layout :expires, :int64_t
+  end
+
+  class UplinkDownloadOptions < FFI::Struct
+    layout :offset, :int64_t,
+           :length, :int64_t
   end
 
   class UplinkListObjectsOptions < FFI::Struct
@@ -80,23 +75,33 @@ module UplinkLib
            :custom, :bool
   end
 
+
+  class UplinkObjectIterator < FFI::Struct
+    layout :_handle, :size_t
+  end
+
+  class UplinkPermission < FFI::Struct
+    layout :allow_download, :bool,
+           :allow_upload, :bool,
+           :allow_list, :bool,
+           :allow_delete, :bool,
+           :not_before, :int64_t,
+           :not_after, :int64_t
+  end
+
+  class UplinkSharePrefix < FFI::Struct
+    layout :bucket, :pointer, # string
+           :prefix, :pointer  # string
+  end
+
   class UplinkError < FFI::Struct
     layout :code, :int32_t,
            :message, :string
   end
 
+
   class UplinkAccessResult < FFI::Struct
     layout :access, UplinkAccess.ptr,
-           :error, UplinkError.ptr
-  end
-
-  class UplinkEncryptionKeyResult < FFI::Struct
-    layout :encryption_key, UplinkEncryptionKey.ptr,
-           :error, UplinkError.ptr
-  end
-
-  class UplinkBucketResult < FFI::Struct
-    layout :bucket, UplinkBucket.ptr,
            :error, UplinkError.ptr
   end
 
@@ -105,10 +110,21 @@ module UplinkLib
            :error, UplinkError.ptr
   end
 
+  class UplinkBucketResult < FFI::Struct
+    layout :bucket, UplinkBucket.ptr,
+           :error, UplinkError.ptr
+  end
+
+  class UplinkObjectResult < FFI::Struct
+    layout :object, UplinkObject.ptr,
+           :error, UplinkError.ptr
+  end
+
   class UplinkUploadResult < FFI::Struct
     layout :upload, UplinkUpload.ptr,
            :error, UplinkError.ptr
   end
+
 
   class UplinkDownloadResult < FFI::Struct
     layout :download, UplinkDownload.ptr,
@@ -125,55 +141,71 @@ module UplinkLib
            :error, UplinkError.ptr
   end
 
-  class UplinkObjectResult < FFI::Struct
-    layout :object, UplinkObject.ptr,
+  class UplinkStringResult < FFI::Struct
+    layout :string, :string,
            :error, UplinkError.ptr
   end
 
+  class UplinkEncryptionKeyResult < FFI::Struct
+    layout :encryption_key, UplinkEncryptionKey.ptr,
+           :error, UplinkError.ptr
+  end
+
+
+  class UplinkUploadObjectMetadataOptions < FFI::Struct; end
+
   attach_function :uplink_parse_access, [:string], UplinkAccessResult.val
-  attach_function :uplink_request_access_with_passphrase, %i[string string string], UplinkAccessResult.val
+  attach_function :uplink_request_access_with_passphrase, [:string, :string, :string], UplinkAccessResult.val
+  attach_function :uplink_access_satellite_address, [UplinkAccess.ptr], UplinkStringResult.val
+  attach_function :uplink_access_serialize, [UplinkAccess.ptr], UplinkStringResult.val
+  attach_function :uplink_access_share, [UplinkAccess.ptr, UplinkPermission.val, :pointer, :long_long], UplinkAccessResult.val
+  attach_function :uplink_access_override_encryption_key, [UplinkAccess.ptr, :string, :string, UplinkEncryptionKey.ptr], UplinkError.ptr
+  attach_function :uplink_free_string_result, [UplinkStringResult.val], :void
   attach_function :uplink_free_access_result, [UplinkAccessResult.val], :void
-
-  attach_function :uplink_open_project, [UplinkAccess.ptr], UplinkProjectResult.val
-  attach_function :uplink_close_project, [UplinkProject.ptr], UplinkError.ptr
-  attach_function :uplink_free_project_result, [UplinkProjectResult.val], :void
-
+  attach_function :uplink_stat_bucket, [UplinkProject.ptr, :string], UplinkBucketResult.val
   attach_function :uplink_create_bucket, [UplinkProject.ptr, :string], UplinkBucketResult.val
   attach_function :uplink_ensure_bucket, [UplinkProject.ptr, :string], UplinkBucketResult.val
-  attach_function :uplink_stat_bucket, [UplinkProject.ptr, :string], UplinkBucketResult.val
   attach_function :uplink_delete_bucket, [UplinkProject.ptr, :string], UplinkBucketResult.val
   attach_function :uplink_delete_bucket_with_objects, [UplinkProject.ptr, :string], UplinkBucketResult.val
   attach_function :uplink_free_bucket_result, [UplinkBucketResult.val], :void
 
-  attach_function :uplink_upload_object, [UplinkProject.ptr, :string, :string, UplinkUploadOptions.ptr], UplinkUploadResult.val
-  attach_function :uplink_free_upload_result, [UplinkUploadResult.val], :void
-  attach_function :uplink_upload_commit, [UplinkUpload.ptr], UplinkError.ptr
-  attach_function :uplink_upload_set_custom_metadata, [UplinkUpload.ptr, UplinkCustomMetadata.val], UplinkError.ptr
-
-  attach_function :uplink_upload_write, [UplinkUpload.ptr, :pointer, :size_t], UplinkWriteResult.val
-  attach_function :uplink_free_write_result, [UplinkWriteResult.val], :void
-  attach_function :uplink_upload_abort, [UplinkUpload.ptr], UplinkError.ptr
-
   attach_function :uplink_download_object, [UplinkProject.ptr, :string, :string, UplinkDownloadOptions.ptr], UplinkDownloadResult.val
-  attach_function :uplink_free_download_result, [UplinkDownloadResult.val], :void
-  attach_function :uplink_close_download, [UplinkDownload.ptr], UplinkError.ptr
-
   attach_function :uplink_download_read, [UplinkDownload.ptr, :pointer, :size_t], UplinkReadResult.val
-  attach_function :uplink_free_read_result, [UplinkReadResult.val], :void
-
-  attach_function :uplink_upload_info, [UplinkUpload.ptr], UplinkObjectResult.val
   attach_function :uplink_download_info, [UplinkDownload.ptr], UplinkObjectResult.val
+  attach_function :uplink_free_read_result, [UplinkReadResult.val], :void
+  attach_function :uplink_close_download, [UplinkDownload.ptr], UplinkError.ptr
+  attach_function :uplink_free_download_result, [UplinkDownloadResult.val], :void
+
+  attach_function :uplink_derive_encryption_key, [:string, :pointer, :size_t], UplinkEncryptionKeyResult.val
+  attach_function :uplink_free_encryption_key_result, [UplinkEncryptionKeyResult.val], :void
+  attach_function :uplink_free_error, [UplinkError.ptr], :void
+
   attach_function :uplink_stat_object, [UplinkProject.ptr, :string, :string], UplinkObjectResult.val
   attach_function :uplink_delete_object, [UplinkProject.ptr, :string, :string], UplinkObjectResult.val
   attach_function :uplink_free_object_result, [UplinkObjectResult.val], :void
   attach_function :uplink_free_object, [UplinkObject.ptr], :void
+
   attach_function :uplink_update_object_metadata, [UplinkProject.ptr, :string, :string, UplinkCustomMetadata.val, UplinkUploadObjectMetadataOptions.ptr], UplinkError.ptr
 
   attach_function :uplink_list_objects, [UplinkProject.ptr, :string, UplinkListObjectsOptions.ptr], UplinkObjectIterator.ptr
   attach_function :uplink_object_iterator_next, [UplinkObjectIterator.ptr], :bool
-  attach_function :uplink_object_iterator_item, [UplinkObjectIterator.ptr], UplinkObject.ptr
   attach_function :uplink_object_iterator_err, [UplinkObjectIterator.ptr], UplinkError.ptr
+  attach_function :uplink_object_iterator_item, [UplinkObjectIterator.ptr], UplinkObject.ptr
   attach_function :uplink_free_object_iterator, [UplinkObjectIterator.ptr], :void
 
-  attach_function :uplink_free_error, [UplinkError.ptr], :void
+  attach_function :uplink_open_project, [UplinkAccess.ptr], UplinkProjectResult.val
+  attach_function :uplink_close_project, [UplinkProject.ptr], UplinkError.ptr
+
+  attach_function :uplink_free_project_result, [UplinkProjectResult.val], :void
+
+  attach_function :uplink_upload_object, [UplinkProject.ptr, :string, :string, UplinkUploadOptions.ptr], UplinkUploadResult.val
+  attach_function :uplink_upload_write, [UplinkUpload.ptr, :pointer, :size_t], UplinkWriteResult.val
+  attach_function :uplink_upload_commit, [UplinkUpload.ptr], UplinkError.ptr
+  attach_function :uplink_upload_abort, [UplinkUpload.ptr], UplinkError.ptr
+  attach_function :uplink_upload_info, [UplinkUpload.ptr], UplinkObjectResult.val
+
+  attach_function :uplink_upload_set_custom_metadata, [UplinkUpload.ptr, UplinkCustomMetadata.val], UplinkError.ptr
+
+  attach_function :uplink_free_write_result, [UplinkWriteResult.val], :void
+  attach_function :uplink_free_upload_result, [UplinkUploadResult.val], :void
 end
